b49ec638a9678d390200fb732910c6c49cc9f670
TakesxiSximada <takesxi.sximada@gmail.com>
2016-10-12 20:36:55 +0900

    gistingを更新

--- templates/gisting.sh
+++ templates/gisting.sh
@@ -20,20 +20,20 @@ gisting() {
 
    if [ "$_GIST_SYNC_URL" ]
    then  # 更新
-       for _GIST_SYNC_TARGET_FILE in `ls $_GIST_SYNC_TARGET_DIR`
+       for _GIST_SYNC_TARGET_FILE in `git ls-files $_GIST_SYNC_TARGET_DIR | grep -v .gitignore`
        do
-           if [ "$(git diff $_GIST_SYNC_TARGET_DIR/$_GIST_SYNC_TARGET_FILE )" ]
+           if [ "$(git diff $_GIST_SYNC_TARGET_FILE )" ]
            then
-               gist -u $_GIST_SYNC_URL -d $_GIST_SYNC_DESCRIPTION $_GIST_SYNC_TARGET_DIR/$_GIST_SYNC_TARGET_FILE | tee $_GIST_SYNC_TARGET_DIR/.gist_url
-               break
+               gist -u $_GIST_SYNC_URL -d $_GIST_SYNC_DESCRIPTION -f `basename $_GIST_SYNC_TARGET_FILE` $_GIST_SYNC_TARGET_FILE | tee $_GIST_SYNC_TARGET_DIR/.gist_url
+               git commit -m "update gist" $_GIST_SYNC_TARGET_FILE
            else
-               echo "No updated:" $_GIST_SYNC_TARGET_DIR/$_GIST_SYNC_TARGET_FILE
+               echo "No updated:" $_GIST_SYNC_TARGET_FILE
            fi
        done
    else  # 登録
        echo gist -d "$_GIST_SYNC_DESCRIPTION" $_GIST_SYNC_TARGET_DIR/*
-       gist -d $_GIST_SYNC_DESCRIPTION $_GIST_SYNC_TARGET_DIR/* | tee $_GIST_SYNC_TARGET_DIR/.gist_url
        git add $_GIST_SYNC_TARGET_DIR
        git commit $_GIST_SYNC_TARGET_DIR -m "updated gist"
+       gist -d $_GIST_SYNC_DESCRIPTION `git ls-files $_GIST_SYNC_TARGET_DIR | grep -v .gitignore` | tee $_GIST_SYNC_TARGET_DIR/.gist_url
    fi
 }
