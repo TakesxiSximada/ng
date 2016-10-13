db99a3b71faeab6afc46019abf951600d8b88c36
TakesxiSximada <takesxi.sximada@gmail.com>
2016-10-13 23:51:09 +0900

    update gisting

--- templates/gisting.sh
+++ templates/gisting.sh
@@ -1,39 +1,32 @@
-gisting() {
-   _GIST_SYNC_TARGET_DIR=$1
-   _GIST_SYNC_URL=
-   _GIST_SYNC_DESCRIPTION="NO TITLE"
+gist-create() {
+    _GIST_DESCRIPTION=$1
+    _GIST_DIRNAME=$2
+    echo "# $_GIST_DESCRIPTION" > /tmp/README.md
+    _GIST_URL=`gist -d "$_GIST_DESCRIPTION" /tmp/README.md`
+    git submodule add $(echo $_GIST_URL | gawk 'match($0, /https:\/\/gist.github.com\/(.+)/, data) {print "git@gist.github.com:" data[1] ".git"}') $_GIST_DIRNAME
+    git submodule update
+    git commit -m "add gist $_GIST_URL"
+    echo $_GIST_DIRNAME
+}
 
-   if [ ! "$_GIST_SYNC_TARGET_DIR" ]
-   then
-      _GIST_SYNC_TARGET_DIR=$(pwd)
-   fi
+gist-create-single() {
+    _GIST_DESCRIPTION=$1
+    echo "$_GIST_DESCRIPTION" > /tmp/README.md
+    _GIST_URL=`gist -d "$_GIST_DESCRIPTION" /tmp/README.md`
+    git clone $(echo $_GIST_URL | gawk 'match($0, /https:\/\/gist.github.com\/(.+)/, data) {print "git@gist.github.com:" data[1] ".git"}')
+}
 
-   if [ -e "$_GIST_SYNC_TARGET_DIR/.gist_url" ]
-   then
-       _GIST_SYNC_URL=$(head -n 1 $_GIST_SYNC_TARGET_DIR/.gist_url | cut -d "/" -f4)
-   fi
+gist-sync() {
+    git commit -m "update gist"
+    git push
+    if [ -e ../.git ]
+    then
+        _GIST_DIRNAME=$(basename $PWD)
+        cd .. && git add $_GIST_DIRNAME && git commit -m "update gist" $_GIST_DIRNAME  && git push
+        cd $_GIST_DIRNAME
+    fi
+}
 
-   if [ -e $_GIST_SYNC_TARGET_DIR/README* ]
-   then
-       _GIST_SYNC_DESCRIPTION=$(head -n 1 $_GIST_SYNC_TARGET_DIR/README* | awk 'sub(/^#[ ]*/, ""); sub(/[\t]/, "");')
-   fi
-
-   if [ "$_GIST_SYNC_URL" ]
-   then  # 更新
-       for _GIST_SYNC_TARGET_FILE in `git ls-files $_GIST_SYNC_TARGET_DIR | grep -v .gitignore`
-       do
-           if [ "$(git diff $_GIST_SYNC_TARGET_FILE )" ]
-           then
-               gist -u $_GIST_SYNC_URL -d $_GIST_SYNC_DESCRIPTION -f `basename $_GIST_SYNC_TARGET_FILE` $_GIST_SYNC_TARGET_FILE | tee $_GIST_SYNC_TARGET_DIR/.gist_url
-               git commit -m "update gist" $_GIST_SYNC_TARGET_FILE
-           else
-               echo "No updated:" $_GIST_SYNC_TARGET_FILE
-           fi
-       done
-   else  # 登録
-       echo gist -d "$_GIST_SYNC_DESCRIPTION" $_GIST_SYNC_TARGET_DIR/*
-       git add $_GIST_SYNC_TARGET_DIR
-       git commit $_GIST_SYNC_TARGET_DIR -m "updated gist"
-       gist -d $_GIST_SYNC_DESCRIPTION `git ls-files $_GIST_SYNC_TARGET_DIR | grep -v .gitignore` | tee $_GIST_SYNC_TARGET_DIR/.gist_url
-   fi
+gist-list() {
+    head -n 1 */README.md
 }
