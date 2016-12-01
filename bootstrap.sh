d49bd6fbc41526da638a1ae53a7407bf5fccff77
TakesxiSximada <takesxi.sximada@gmail.com>
2016-12-01 18:24:53 +0900

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,22 @@
 #! /usr/bin/env bash
+which brew > /dev/null
+if [ "0" != "$?" ]
+then
+    echo "Failed: Need homebrew. See http://brew.sh/"
+    exit 1
+fi
+
+
+which go > /dev/null
+if [ "0" != "$?" ]
+then
+    echo "Failed: Need Golang. \`brew install go\`"
+    exit 1
+fi
+
+GOPATH=$PWD
+NG_ROOT=$GOAPTH/src/TakesxiSximada/ng/ng
+
+go get -u -d github.com/TakesxiSximada/ng
+ln -sf $NG_ROOT/ng.mk.example Makefile
+make create
